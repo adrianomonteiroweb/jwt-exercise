378cc963aa93ab50c3b00e15ea576dfb54e17b5d
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-01-17 10:34:11 -0300

    login token

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
+
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
 const { loginService } = require('../services/users.services');
 
-const loginController = async (req, res) => {
+const bodyValidation = (body) =>
+  Joi.object({
+    username: Joi.string().min(5).alphanum().required(),
+    password: Joi.string().min(5).required(),
+  }).validate(body);
+
+const loginController = async (req, res, next) => {
+  const { error } = bodyValidation(req.body);
+  if (error) return next(error);
   let result;
   try {
     result = await loginService(req.body);
-  } catch (error) {
+  } catch (err) {
     return res.status().json({ message: 'Erro na conexão!' });
   }
-  return result
-  ? res.status(200).json(result.ops.pop())
+  const payload = {
+    username: req.body.username,
+    admin: false,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  return result ? res.status(200).json({ token })
   : res.status(422).json({ message: 'Erro ao realizar login!' });
 };
 
