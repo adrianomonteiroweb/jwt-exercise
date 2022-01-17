9cd016b1fc1f50bbe31ddcd2c8cf48172f12386f
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-01-17 12:41:29 -0300

    admin token

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -7,31 +7,27 @@ const { JWT_SECRET } = process.env;
 
 const { loginService } = require('../services/users.services');
 
+const loginError = { message: 'Erro ao realizar login!' };
+
 const bodyValidation = (body) =>
   Joi.object({
     username: Joi.string().min(5).alphanum().required(),
     password: Joi.string().min(5).required(),
   }).validate(body);
 
+const newError = new Error('Invalid username or password');
+newError.statusCode = 401;
+
 const loginController = async (req, res, next) => {
+  const { username, password } = req.body;
+  const admin = username === 'admin' && password !== 's3nh4S3gur4???';
   const { error } = bodyValidation(req.body);
   if (error) return next(error);
-  let result;
-  try {
-    result = await loginService(req.body);
-  } catch (err) {
-    return res.status().json({ message: 'Erro na conexão!' });
-  }
-  const payload = {
-    username: req.body.username,
-    admin: false,
-  };
-
-  const token = jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '1h',
-  });
-  return result ? res.status(200).json({ token })
-  : res.status(422).json({ message: 'Erro ao realizar login!' });
+  if (username === 'admin' && password !== 's3nh4S3gur4???') return next(newError);
+  const result = await loginService(req.body);
+  const payload = { username, admin };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+  return result ? res.status(200).json({ token }) : res.status(422).json(loginError);
 };
 
 module.exports = {
